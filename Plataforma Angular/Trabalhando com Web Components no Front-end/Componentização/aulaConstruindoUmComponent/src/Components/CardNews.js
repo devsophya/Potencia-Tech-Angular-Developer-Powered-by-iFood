a1ed5a5cd1d61c56d6f74e145f408b1da25ca269
Sophya <148731999+devsophya@users.noreply.github.com>
2024-10-07 18:45:22 -0300

    aulaConstruindoUmComponent - concluída com êxito.
    aulaConstruindoUmComponent - concluída com êxito.

---
+++ Plataforma Angular/Trabalhando com Web Components no Front-end/Componentização/aulaConstruindoUmComponent/src/Components/CardNews.js
@@ -0,0 +1,14 @@
+class CardNews extends HTMLElement {
+    constructor() {
+        super();
+
+        const shadow = this.attachShadow({mode: "open"});
+        shadow.innerHTML = "<h1>Hello</h1>";
+    }
+
+    build() {}
+
+    styles() {}
+}
+
+customElements.define('card-news',CardNews);
